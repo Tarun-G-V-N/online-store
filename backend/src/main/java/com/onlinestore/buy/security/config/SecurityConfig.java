8f9bc3968a8010c2fcae65fcd8f2be5c8e954449
Tarun <tarungandikota9012@gmail.com>
2026-01-28 18:58:44 +0530

    added vercel endpoint for CORS

--- backend/src/main/java/com/onlinestore/buy/security/config/SecurityConfig.java
+++ backend/src/main/java/com/onlinestore/buy/security/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5000", "http://online-store-client.s3-website.ap-south-1.amazonaws.com/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5000", "http://online-store-client.s3-website.ap-south-1.amazonaws.com/", "https://online-store-black-beta.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
